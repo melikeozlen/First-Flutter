f4d41c0364af71a6ab73067a974fc061dab3f86e
melikeozlen <melikeozlen@gmail.com>
2023-08-08 21:39:29 +0300

    learn state

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,58 @@ void main() {
   runApp(const WidgetDetail());
 }
 
-class WidgetDetail extends StatelessWidget {
+class WidgetDetail extends StatefulWidget {
   const WidgetDetail({super.key});
+
+  @override
+  State<WidgetDetail> createState() => WidgetDetailState();
+}
+
+class WidgetDetailState extends State<WidgetDetail> {
+  int count = 0;
+  bool visible = false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("First Flutter App"),
-          centerTitle: true, // Başlığı ortaladı
-          backgroundColor: Colors.amber,
-          leading: const Text("LOGO"),
-        ),
-        body: const Center(
-          child: Text("Hello"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Text("+"),
-          onPressed: () {},
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Count: " + count.toString(),
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    count = count + 1;
+                    if (count >= 0) {
+                      visible = false;
+                    }
+                  });
+                },
+                child: const Text("+"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    if (count == 0) {
+                      visible = true;
+                    } else {
+                      count = count - 1;
+                      visible = false;
+                    }
+                  });
+                },
+                child: const Text("-"),
+              ),
+              Visibility(
+                visible: visible,
+                child: const Text("Count cannot less than 0"),
+              )
+            ],
+          ),
         ),
       ),
     );
